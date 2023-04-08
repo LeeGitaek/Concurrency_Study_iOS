882cd6afa556029cbf3537c3ceeca7a2a1830c14
LeeGiTaek <you@example.com>
2023-04-08 10:42:49 -0700

    Initial Commit

---
+++ SampleProject/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  SampleProject
+//
+//  Created by gitaeklee on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
